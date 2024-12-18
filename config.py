9367aab14f9f36c9280e2eb558ef6899f7fe5cbf
goat587 <goatgoat0987@gmail.com>
2024-12-18 15:26:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ class Config:
     API_HASH = environ.get("API_HASH", "1b4cdb255f37262200981dbbf87a1fa0")
     BOT_TOKEN = environ.get("BOT_TOKEN", "8096179406:AAGvFyNW9O9O994VxrlR_aPkZj1RiroWA3M") 
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
-    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://chhjgjkkjhkjhkjh@cluster0.xowzpr4.mongodb.net/")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "forward-bot")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6964148334').split()]
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://bxbxj1280:kP47C0uyPOXRHH7w@cluster0.whdju.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "bxbxj1280")
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '7413628447').split()]
 
 class temp(object): 
     lock = {}
